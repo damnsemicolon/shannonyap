395c9e70248bd8971aa7337bb7e5e1231b9fe6aa
damnsemicolon <shannoninengland@gmail.com>
2023-03-13 20:03:52 +0000

    navbar coded for small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav
@@ -41,8 +42,32 @@ const Navbar = () => {
           ))}
         </ul>
          {/* link for sm size screens */}
-        <div>
-
+        <div className='sm:hideen flex flex-1 justify-end items-center'>
+                <img
+                  src={toggle ? close : menu}
+                  alt="menu"
+                  className='w-[28px] h-p28px] object-contain cursor-pointer'
+                  onClick={() => setToggle(!toggle)}
+                />
+<div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+<ul className='list-none flex ustify-end items-start flex-col gap-4'>
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } font-poppins font-medium cursor-pointer text-[16px]`}
+                onClick={() => {
+                setToggle(!toggle);
+                setActive(link.title);
+                }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+</div>
         </div>
       </div>
     </nav>
