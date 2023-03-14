abd9397869d45ba7acddd2743002d07aa95d6f73
damnsemicolon <shannoninengland@gmail.com>
2023-03-14 19:07:32 +0000

    set up cards for skills section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,17 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+const ServiceCard = ({ index, title, icon }) => {
+  return(
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div 
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
 
+      </motion.div>
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
