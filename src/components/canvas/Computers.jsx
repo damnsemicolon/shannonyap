3320035d590c0dd98dc4aded5b523ba3644519e0
damnsemicolon <shannoninengland@gmail.com>
2023-03-14 17:10:16 +0000

    added 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./enterprise/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={1}
+        groundColor='black' />
+      {/* <pointLight intensity={1} /> */}
+      <primitive
+        object={computer.scene}
+        scale={0.2}
+        position={[6, -1, 6]}
+        rotation={[-0.1, -0.75, 0.5]}
+      />
+
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+export default ComputersCanvas
