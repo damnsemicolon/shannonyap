bab160822dd34f97afff722fb6f9c5d7f5ce6995
damnsemicolon <shannoninengland@gmail.com>
2023-03-15 19:23:40 +0000

    structure for Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import {styles} from '../styles';
+import { github } from '../assets';
+import {SectionWrapper } from '../hoc';
+import { projects } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Fingers crossed hope it still loads</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          varients={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+            Following projects showcases my skills and experience throughout my never-ending coding journey. Hover over the cards for more details.
+          </motion.p>
+          <div className='mt-20 flex'>
+
+          </div>
+      </div>
+    </>
+      )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
