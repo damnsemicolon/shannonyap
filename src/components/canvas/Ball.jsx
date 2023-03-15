b684087b6d0927f801a361bb1ca8af8bfaeb75a2
damnsemicolon <shannoninengland@gmail.com>
2023-03-15 18:27:16 +0000

    coded to render calls to show skill icons

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,8 +10,23 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh cashShadow receiveShadow scale={2.75}>
+      {/* sides of the icoshahedron */}
+        <icosahedronGeometry args={[1, 2]} />
+        <meshStandardMaterial 
+          color='#fff8eb' 
+          polygonOffset 
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+        position={[0,0,1]}
+        rotation={ [2 * Math.PI, 0, 6.25]}
+        map={decal} />
+      </mesh>
     </Float>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -70,18 +70,18 @@ import {
       name: "JavaScript",
       icon: javascript,
     },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
+    // {
+    //   name: "TypeScript",
+    //   icon: typescript,
+    // },
     {
       name: "React JS",
       icon: reactjs,
     },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
     {
       name: "Tailwind CSS",
       icon: tailwind,
@@ -90,10 +90,10 @@ import {
       name: "Node JS",
       icon: nodejs,
     },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
+    // {
+    //   name: "MongoDB",
+    //   icon: mongodb,
+    // },
     {
       name: "Three JS",
       icon: threejs,
@@ -102,14 +102,14 @@ import {
       name: "git",
       icon: git,
     },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
+    // {
+    //   name: "figma",
+    //   icon: figma,
+    // },
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
   ];
   
   const experiences = [
