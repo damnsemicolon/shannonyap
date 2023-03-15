b22de6684b7bf550ec5c6b464e3604ee65e2b1fe
damnsemicolon <shannoninengland@gmail.com>
2023-03-15 19:52:53 +0000

    coded for cards in Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,28 @@
 import Tilt from 'react-tilt';
 import { motion } from 'framer-motion';
 
-import {styles} from '../styles';
+import { styles } from '../styles';
 import { github } from '../assets';
-import {SectionWrapper } from '../hoc';
+import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+
+const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 1)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+
+      </Tilt>
+    </motion.div>
+  )
+}
 
 const Works = () => {
   return (
@@ -17,16 +34,23 @@ const Works = () => {
 
       <div className='w-full flex'>
         <motion.p
-          varients={fadeIn("", "", 0.1, 1)}
+          variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-            Following projects showcases my skills and experience throughout my never-ending coding journey. Hover over the cards for more details.
-          </motion.p>
-          <div className='mt-20 flex'>
+          Following projects showcases my skills and experience throughout my never-ending coding journey. Hover over the cards for more details.
+        </motion.p>
+      </div>
 
-          </div>
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={`project-${index}`}
+            index={index}
+            {...project}
+          />
+        ))}
       </div>
     </>
-      )
+  )
 }
 
 export default SectionWrapper(Works, "");
