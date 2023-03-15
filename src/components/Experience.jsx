e09b06c6c7136b459f0f8932b84771344d41eec9
damnsemicolon <shannoninengland@gmail.com>
2023-03-15 16:31:00 +0000

    polishing for Experince.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,10 +13,9 @@ const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{ background: '#1d1836', color: '#fff' }}
     contentArrowStyle={{ borderRight: '15px solid #1d1836' }}
-    date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className='flex justify-center items-center w-full h-full'>
         <img
           src={experience.icon}
           alt={experience.company_name}
@@ -26,12 +25,22 @@ const ExperienceCard = ({ experience }) => (
     }
   >
     <div>
-      <h3 className='text-white text-[24px] font-bold'>
-        {experience.title}
-      </h3>
+      <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+      <p className='text-secondary text-[16px]' style={{ margin: 0 }}>{experience.date}</p>
+      <p className='text-secondary text-[16px]' style={{ margin: 0 }}>{experience.company_name}</p>
     </div>
+    <ul className='mt-5 list-disc ml-5 space-y-2'>
+      {experience.points.map((point, index) => (
+        <li key={`experience-point-${index}`}
+        className='text-white-100 text-[14px] pl-1 tracking-wider'
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 )
+
 const Experience = () => {
   return (
     <>
