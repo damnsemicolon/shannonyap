9f885aeb71f6903aa1bf83614e1c121b67db4317
damnsemicolon <shannoninengland@gmail.com>
2023-03-16 16:44:22 +0000

    added tags for technology used

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -24,7 +24,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
             alt={name}
             className='w-full h-full object-cover rounded-2xl' />
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-          {/* GITHUB LINK */}
+        {/* Github Button */}
             <div
               onClick={() => window.open(source_code_link, '_blank')}
               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
@@ -33,7 +33,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
                 alt='github'
                 className='w-1/2 h-1/2 object-contain hover:scale-150 transition-transform duration-300 ease-in-out' />
             </div>
-            {/* DEPLOYED LINK */}
+        {/* Deployed Button */}
             <div
               onClick={() => window.open(deployed_link, '_blank')}
               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'>
@@ -45,7 +45,20 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link, 
           </div>
         </div>
 
+        {/* Project name and descriptions */}
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
 
+        {/* Tags for technology used */}
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </Tilt>
     </motion.div>
   )
