60de6d8286bee5e7096c5d50cc9a5428bf78c64c
damnsemicolon <shannoninengland@gmail.com>
2023-03-17 01:05:51 +0000

    polished balls again

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,27 +1,23 @@
-import React, { Suspense, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { 
+  Decal, Float, OrbitControls, Preload, useTexture 
+} from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  const [rotation, setRotation] = useState([0, 0, 0]);
-
-  useFrame((state, delta) => {
-    setRotation([rotation[0], rotation[1] + delta * 0.5, rotation[2] + delta * 0.25]);
-  });
-  
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh cashShadow receiveShadow scale={2.75} rotation={rotation}>
+      <mesh scale={3}>
       {/* sides of the icoshahedron */}
         <icosahedronGeometry args={[1, 2]} />
         <meshStandardMaterial 
-          color='#fff8eb' 
+          color='#fff8eb'
           flatShading
         />
         <Decal 
@@ -33,7 +29,7 @@ const Ball = (props) => {
   )
 }
 
-const BallCanvas = ({ icon}) => {
+const BallCanvas = ({ icon }) => {
   return(
     <Canvas
       frameloop='demand'
@@ -49,4 +45,4 @@ const BallCanvas = ({ icon}) => {
   )
 }
 
-export default BallCanvas;
+export default BallCanvas
